7a898e1a5b75318b513f291bdd867d1a02ea077c
Anish Nath <anish@stardust.gg>
2025-04-28 20:16:50 +0530

    Modified scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/temp-email
 go build -o bin/temp-email ./cmd/api
 go build -o bin/process-email ./cmd/email-processor
 go build -o bin/dummy-data ./cmd/dummy-data
-sudo mv bin/process-email ~/usr/local/bin/
+sudo mv bin/process-email /usr/local/bin/
 sudo cp config/postfix/* /etc/postfix/
 sudo postmap /etc/postfix/virtual
 sudo cp config/procmailrc /home/ubuntu/.procmailrc
